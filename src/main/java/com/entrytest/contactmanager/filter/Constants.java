d331d7ce2dad30a25b777b8baa9d0f9d102b56c8
HoangDHCodeGym <ol.hoangdh.thna@gmail.com>
2018-12-05 22:58:09 +0700

    Add JWT Service

---
+++ src/main/java/com/entrytest/contactmanager/filter/Constants.java
@@ -0,0 +1,8 @@
+package com.entrytest.contactmanager.filter;
+
+public class Constants {
+    public static final long EXPIRATION_TIME = 60*60*24*1000;
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER = "Authorization";
+    public static final String SECRET = "SomeSecretCodes";
+}

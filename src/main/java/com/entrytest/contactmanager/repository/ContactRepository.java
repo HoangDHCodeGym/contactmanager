481eaad9ed91534634348930ddc5c480442fa2ec
HoangDHCodeGym <ol.hoangdh.thna@gmail.com>
2018-12-05 22:46:59 +0700

    Initial commit

---
+++ src/main/java/com/entrytest/contactmanager/repository/ContactRepository.java
@@ -0,0 +1,12 @@
+package com.entrytest.contactmanager.repository;
+
+import com.entrytest.contactmanager.model.Contact;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "contacts", path = "contacts")
+public interface ContactRepository extends PagingAndSortingRepository<Contact, Long> {
+    List<Contact> findAll();
+}
